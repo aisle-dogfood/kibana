a4f3bbc1a98a4c40621d0a44212a8a32797b1ae8
weareaisle-qa-app <weareaisle-qa-app[bot]@users.noreply.github.com>
2026-06-05 13:47:30 +0000

    Apply fix for issue: TLS Certificate Verification Disabled in Email Connector

--- x-pack/platform/plugins/shared/stack_connectors/server/connector_types/email/send_email.ts
+++ x-pack/platform/plugins/shared/stack_connectors/server/connector_types/email/send_email.ts
@@ -267,11 +267,6 @@ function getTransportConfig(
       );
       transportConfig.proxy = proxySettings.proxyUrl;
       transportConfig.headers = proxySettings.proxyHeaders;
-    } else if (!transportConfig.secure && user == null && password == null) {
-      // special case - if secure:false && user:null && password:null set
-      // rejectUnauthorized false, because simple/test servers that don't even
-      // authenticate rarely have valid certs; eg cloud proxy, and npm maildev
-      transportConfig.tls = { rejectUnauthorized: false };
     } else {
       transportConfig.tls = getNodeSSLOptions(logger, generalSSLSettings.verificationMode);
     }
